8d9b2166fcd8d4ecbcd6261652d0ced41dc56f74
Nikhil-S98 <124851112+Nikhil-S98@users.noreply.github.com>
2023-08-05 00:42:59 -0400

    Created an array to store header data, and an array that stores nested arrays for any number of body rows

--- scripts/converter.js
+++ scripts/converter.js
@@ -1,24 +1,45 @@
+// Description: This file contains the functions that convert html to yaml and vice versa
+
+//Function to get string data from "htmlInput" and send it to converter function
+//Then it will take the output from the converter function and put it in "yamlOutput" in index.html
 function convertToYAML() {
     const htmlInput = document.getElementById("htmlInput").value;
     const yamlOutput = converter(htmlInput);
     document.getElementById("yamlOutput").value = yamlOutput;
 }
 
-
+//Function to get get string data "htmlInput" formatted as table data and convert it into YAML format
 function converter(htmlInput) {
 
     //Temporary div element to store htmlInput so that querySelector can be used
     const tempDiv = document.createElement("div");
     tempDiv.innerHTML = htmlInput;
 
-    // Get the table from the htmlInput
+    //Create tempDiv to store htmlInput so that querySelector can be used
     const table = tempDiv.querySelector("table");
     const headers = table.querySelectorAll("thead th");
 
+    //Create an array to store the header values
     const headerValues = [];
 
     //Loop through the th elements and add their text content to the array
     for (let i=0; i < headers.length; i++) {
         headerValues.push(headers[i].textContent);
     }
+
+    //Select rows from the table and crate an array of rows
+    const rows = table.querySelectorAll("tbody tr");
+    rowArrays = [];
+
+    //Create an array for each row and push it back into rowArrays
+    for (let i = 0; i < rows.length; i++) {
+        const cells = rows[i].querySelectorAll("td");
+        const rowValues = [];
+
+        for (let j = 0; j < cells.length; j++) {
+            rowValues.push(cells[j].textContent);
+        }
+
+        rowArrays.push(rowValues);
+    }   
 }
