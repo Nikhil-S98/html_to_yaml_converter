71286e1d3b6f297eb4b0f38b332810e1cbd08280
Nikhil-S98 <124851112+Nikhil-S98@users.noreply.github.com>
2023-08-04 23:52:44 -0400

    Created array to store any inputted headers

--- scripts/converter.js
+++ scripts/converter.js
@@ -7,8 +7,18 @@ function convertToYAML() {
 
 function converter(htmlInput) {
 
+    //Temporary div element to store htmlInput so that querySelector can be used
+    const tempDiv = document.createElement("div");
+    tempDiv.innerHTML = htmlInput;
 
+    // Get the table from the htmlInput
+    const table = tempDiv.querySelector("table");
+    const headers = table.querySelectorAll("thead th");
 
+    const headerValues = [];
 
-    
-}   
+    //Loop through the th elements and add their text content to the array
+    for (let i=0; i < headers.length; i++) {
+        headerValues.push(headers[i].textContent);
+    }
+}
